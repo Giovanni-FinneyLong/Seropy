dc7815a13fd437c96106434f7788d096781a66c9
Giovanni Finney-Long <gfinneylong@gmail.com>
2015-07-10 19:10:41 -0700

    Created method for AP Clustering. Fails on numpy arrays that are a result of K-means clustering for unknown reasons. Am now capable of doing multi-layer clustering however.
    Next step is to add a k-means function that is the simlar to the other clusterings' methods; printing the results. This will assist in visualization for now.
    
    Would probably be nice to create a method to turn lists of pixels or lists of lists of pixels into arrays.

--- image-analysis/sero.py
+++ image-analysis/sero.py
@@ -86,20 +86,19 @@ def findBestClusterCount(min, max, step):
     plt.title('Elbow method for K-Means Clustering on Pixels\nManually Record the desired K value\n')
     plt.show()
 
-def getClusterLists(num_clusters):
+def KMeansClusterIntoList(array_in, num_clusters):
+    'Take an array of floats and returns a list of lists, each of which contains all the pixels of a cluster'
     cluster_lists = [[] for i in range(num_clusters)]
     # for c in range(num_clusters):
     #     cluster_arrays.append(zeros([xdim, ydim])) # (r,c)
-    (bookC, distortionC)  = kmeans(max_pixel_array_floats, num_clusters)
-    (centLabels, centroids) = vq(max_pixel_array_floats, bookC)
+    (bookC, distortionC)  = kmeans(array_in, num_clusters)
+    (centLabels, centroids) = vq(array_in, bookC)
     for pixlabel in range(len(centLabels)):
         cluster = centLabels[pixlabel]
         # pix = max_pixel_array_floats[pixlabel]
         # cluster_arrays[cluster][pix[1]][pix[2]] = pix[0]
-        cluster_lists[cluster].append(max_pixel_array_floats[pixlabel])
-    print('Done populating array for each cluster')
+        cluster_lists[cluster].append(array_in[pixlabel])
     return cluster_lists
-    # TODO convert to sparse
 
 def MeanShiftCluster(array_in):
     'Does mean shift clustering on an array of max_points, does NOT take lists'
@@ -115,17 +114,56 @@ def MeanShiftCluster(array_in):
     print("number of estimated clusters : %d" % n_clusters_)
 
     #PLOTTING THE RESULT
-    plt.figure(1)
+    plt.figure(2)
     plt.clf()
     colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
     for k, col in zip(range(n_clusters_), colors):
         my_members = labels == k
         cluster_center = cluster_centers[k]
-        plt.plot(max_pixel_array_floats[my_members, 1], max_pixel_array_floats[my_members, 2], col + '.')
-        plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+        avoid_output = plt.plot(max_pixel_array_floats[my_members, 1], max_pixel_array_floats[my_members, 2], col + '.')
+        avoid_output2 = plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
                  markeredgecolor='k', markersize=14)
     plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.show()
+
+def AffinityPropagationCluster(array_in):
+    af = AffinityPropagation(preference=-50).fit(array_in)
+    cluster_centers_indices = af.cluster_centers_indices_
+    labels = af.labels_
+    n_clusters_ = len(cluster_centers_indices)
+    print('Estimated number of clusters: %d' % n_clusters_)
+    # print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    # print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    # print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    # print("Adjusted Rand Index: %0.3f"
+    #       % metrics.adjusted_rand_score(labels_true, labels))
+    # print("Adjusted Mutual Information: %0.3f"
+    #       % metrics.adjusted_mutual_info_score(labels_true, labels))
+    print("Silhouette Coefficient: %0.3f"
+          % metrics.silhouette_score(array_in, labels, metric='sqeuclidean'))
+    #Plot results
+    # plt.close('all')
+    plt.figure(1)
+    plt.clf()
+
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+    for k, col in zip(range(n_clusters_), colors):
+        class_members = labels == k
+        cluster_center = array_in[cluster_centers_indices[k]]
+        plt.plot(array_in[class_members, 1], array_in[class_members, 2], col + '.')
+        plt.plot(cluster_center[1], cluster_center[2], 'o', markerfacecolor=col,
+                 markeredgecolor='k', markersize=14)
+        for x in array_in[class_members]:
+            plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
+    # TODO NOTE that this process takes 9GB ram for 1M points, and that the demo was only on 300..
+    # TODO Took 15 Minutes before estimating the # of clusters, unloading 5GB of RAM,  then encountering an invalid value by getting the mean of an empty slice
+    #   IE, this method isn't scalable to use on an entire image of 2.56M pixels..
+    #   Perhaps can use K-Means to reduce the points, and then do secondary clustering with above/else?
+    #   If the k-means was done loosely, it would have the effect of grouping pixels into neighborhoods
+
 
 
 
@@ -165,11 +203,8 @@ for s in range(len(image_channels)): # Better to split image and use splits for 
     norm_slices.append(255 * buf / np.linalg.norm(buf))
     if (np.amax(slices[s]) == 0):
         print('Slice #' + str(s) + ' is an empty slice')
-
-
-im = Image.fromarray(np.uint8(cm.jet(slices[0])*255))
+# im = Image.fromarray(np.uint8(cm.jet(slices[0])*255))
 # out = im.filter(ImageFilter.MaxFilter)
-
 # Can use im.load as needed to access Image pixels
 # Opting to use numpy, I expect this will be faster to operate and easier to manipulate
 
@@ -196,33 +231,30 @@ print('There are  ' + str(endmax) + ' maximal pixels')
 max_pixel_list = sorted_pixels[0:endmax] # Pixels with value 255
 max_pixel_array_floats = np.asarray([(float(i[0]), float(i[1]), float(i[2])) for i in max_pixel_list])
 
-print('mpshape:' + str(max_pixel_array_floats.shape))
-
 # findBestClusterCount(0, 100, 5)
 
 # Now have labels in centLabels for each of the max_pixels
 # For fun, going to make an array for each cluster
 
-
 cluster_count = 20
-cluster_lists = getClusterLists(cluster_count)
+cluster_lists = KMeansClusterIntoList(max_pixel_array_floats, cluster_count)
 for i in range(len(cluster_lists)):
     print('Index:' + str(i) + ', size:' + str(len(cluster_lists[i]))) # + ' pixels:' + str(cluster_lists[i]))
-# Now to start working on each cluster, and see if we can generate some blobs!!! :D
-'''
-Rules:
-    A max pixel (mp) has value 255
-    Around a pixel means the 8 pixels that are touching it in the 2d plane
-        123
-        4.5
-        678
-    Any mp next to each other belong together
-    Any mp that has no mp around it is removed as noise
-    TODO: https://books.google.com/books?id=ROHaCQAAQBAJ&pg=PA287&lpg=PA287&dq=python+group+neighborhoods&source=bl&ots=f7Vuu9CQdg&sig=l6ASHdi27nvqbkyO_VvztpO9bRI&hl=en&sa=X&ei=4COgVbGFD8H1-QGTl7aABQ&ved=0CCUQ6AEwAQ#v=onepage&q=python%20group%20neighborhoods&f=false
-        Info on neighborhoods
-'''
+
 
 # MeanShiftCluster(max_pixel_array_floats)
+# AffinityPropagationCluster(max_pixel_array_floats):
+cluster_arrays = [] # Each entry is an array, filled only with the maximal values from the corresponding
+for cluster in range(cluster_count):
+    cluster_arrays.append(zeros([xdim, ydim])) # (r,c)
+    for pixel in cluster_lists[cluster]:
+        cluster_arrays[cluster][pixel[1]][pixel[2]] = pixel[0]
+
+
+
+
+sub_cluster_count = 10
+
 
 
 
@@ -236,45 +268,21 @@ Rules:
 # for (p_num, pixel) in enumerate(sorted_pixels):
 #     print(str(p_num) + ': ' + str(pixel))
 
-af = AffinityPropagation(preference=-50).fit(max_pixel_array_floats)
-cluster_centers_indices = af.cluster_centers_indices_
-labels = af.labels_
-n_clusters_ = len(cluster_centers_indices)
-print('Estimated number of clusters: %d' % n_clusters_)
-# print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-# print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-# print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-# print("Adjusted Rand Index: %0.3f"
-#       % metrics.adjusted_rand_score(labels_true, labels))
-# print("Adjusted Mutual Information: %0.3f"
-#       % metrics.adjusted_mutual_info_score(labels_true, labels))
-print("Silhouette Coefficient: %0.3f"
-      % metrics.silhouette_score(max_pixel_array_floats, labels, metric='sqeuclidean'))
-#Plot results
-# plt.close('all') 
-plt.figure(1)
-plt.clf()
-
-colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
-for k, col in zip(range(n_clusters_), colors):
-    class_members = labels == k
-    cluster_center = max_pixel_array_floats[cluster_centers_indices[k]]
-    plt.plot(max_pixel_array_floats[class_members, 1], max_pixel_array_floats[class_members, 2], col + '.')
-    plt.plot(cluster_center[1], cluster_center[2], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-    for x in max_pixel_array_floats[class_members]:
-        plt.plot([cluster_center[0], x[0]], [cluster_center[1], x[1]], col)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-plt.show()
-# TODO NOTE that this process takes 9GB ram for 1M points, and that the demo was only on 300..
-# TODO Took 15 Minutes before estimating the # of clusters, unloading 5GB of RAM,  then encountering an invalid value by getting the mean of an empty slice
-#   IE, this method isn't scalable to use on an entire image of 2.56M pixels..
-#   Perhaps can use K-Means to reduce the points, and then do secondary clustering with above/else?
-#   If the k-means was done loosely, it would have the effect of grouping pixels into neighborhoods
-
-
 runShell()
 
 # plt.imsave for saving
 #TODO recover and use more of the data from kmeans, like the centroids, which can be used to relate blobs?
+
+# Now to start working on each cluster, and see if we can generate some blobs!!! :D
+'''
+Rules:
+    A max pixel (mp) has value 255
+    Around a pixel means the 8 pixels that are touching it in the 2d plane
+        123
+        4.5
+        678
+    Any mp next to each other belong together
+    Any mp that has no mp around it is removed as noise
+    TODO: https://books.google.com/books?id=ROHaCQAAQBAJ&pg=PA287&lpg=PA287&dq=python+group+neighborhoods&source=bl&ots=f7Vuu9CQdg&sig=l6ASHdi27nvqbkyO_VvztpO9bRI&hl=en&sa=X&ei=4COgVbGFD8H1-QGTl7aABQ&ved=0CCUQ6AEwAQ#v=onepage&q=python%20group%20neighborhoods&f=false
+        Info on neighborhoods
+'''
